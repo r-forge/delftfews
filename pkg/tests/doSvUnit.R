a5dc8f6f96e37a544a3accd1a37177a3ebed5a00
mariotomo (drs ) <mariotomo@gmail.com>
2010-10-01 12:19:34 +0000

    renaming the directory to the default value (in the hope it will be seen by R CMD check) and helping doSvUnit to really find them!
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/delftfews@46 9204a4a8-c410-4640-99f9-becd67d350d5

--- pkg/tests/doSvUnit.R
+++ pkg/tests/doSvUnit.R
@@ -2,6 +2,6 @@
 require(svUnit)  # Needed if run from R CMD BATCH
 require(delftfews)
 unlink("report.xml")  # Make sure we generate a new report
-mypkgSuite <- svSuiteList("delftfews", dirs="../inst/RUnit")  # List all our test suites
+mypkgSuite <- svSuiteList("delftfews", dirs="../../pkg/inst/unitTest")  # List all our test suites
 runTest(mypkgSuite, name = "delftfews")  # Run them...
 protocol(Log(), type = "junit", file = "report.xml")  # ... and write report
