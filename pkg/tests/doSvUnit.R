dc65d8f0682707be55afd27e63eaddad5200d0c6
mariotomo (drs ) <mariotomo@gmail.com>
2011-09-09 13:30:40 +0000

    work in progress, do not use this version!
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/delftfews@113 9204a4a8-c410-4640-99f9-becd67d350d5

--- pkg/tests/doSvUnit.R
+++ pkg/tests/doSvUnit.R
@@ -5,5 +5,5 @@ require(delftfews)
 unlink("report.xml")  # Make sure we generate a new report
 mypkgSuite <- svSuiteList("delftfews", dirs="../../pkg/inst/unitTest")  # List all our test suites
 runTest(mypkgSuite, name = "delftfews")  # Run them...
-runTest(makeTestListFromExamples("delftfews", "../../pkg/man/"))
+## runTest(makeTestListFromExamples("delftfews", "../../pkg/man/"))
 protocol(Log(), type = "junit", file = "report.xml")  # ... and write report
