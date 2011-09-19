82ddaf184185002be827b243bee36e971ecdf7ee
mariotomo (drs ) <mariotomo@gmail.com>
2011-09-19 10:56:24 +0000

    promoting repeated string to local variable
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/delftfews@116 9204a4a8-c410-4640-99f9-becd67d350d5

--- pkg/tests/doSvUnit.R
+++ pkg/tests/doSvUnit.R
@@ -1,9 +1,11 @@
 #! /usr/bin/Rscript
 require(svUnit)  # Needed if run from R CMD BATCH
+
+pkg <- "delftfews"
 require(delftfews)
 
 unlink("report.xml")  # Make sure we generate a new report
-mypkgSuite <- svSuiteList("delftfews", dirs="../../pkg/inst/unitTest")  # List all our test suites
-runTest(mypkgSuite, name = "delftfews")  # Run them...
-runTest(makeTestListFromExamples("delftfews", "../../pkg/man/"))
+mypkgSuite <- svSuiteList(pkg, dirs="../../pkg/inst/unitTest")  # List all our test suites
+runTest(mypkgSuite, name = pkg)  # Run them...
+runTest(makeTestListFromExamples(pkg, "../../pkg/man/"))
 protocol(Log(), type = "junit", file = "report.xml")  # ... and write report
